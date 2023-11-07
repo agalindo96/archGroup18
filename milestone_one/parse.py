e7748a1dbd2ebf1baa91a85488469e4207b8b908
me <91627805+agalindo96@users.noreply.github.com>
2023-11-07 16:43:18 -0600

    New branch, changes to parser

--- milestone_one/parse.py
+++ milestone_one/parse.py
@@ -1,18 +1,22 @@
 import argparse
 import re
 
+import part2
+
 """
     We're gonna need to process physical memory size arg as a string since the upper range 
     would be way too long to manually enter (512GB = 536,870,912KB)
 
     To validate we're gonna have to translate the arg to an integer and compare against the lower/upper range
 
-    Figure how to structure this. Create executable? Driver script?
+    Figure how to structure this. Create executable?
 
-    TODO: Validate filename args, error and exit if file does not exist
-        Maybe we should do that error check in the next part of the program
+    TODO: Validate filename args in the next part of the program
         i.e. this script gives a list of all -f args to the next script, which processes each arg
              if the file does not exist, print an error message but still process the valid files
+
+        TODO: I think it makes more sense for THIS to be the main file, which then passes the parsed args to
+              the next part of the program
 
     TODO: Make all arguments required, currently filename works any number of args (including 0)
         argparse help groups?
@@ -33,7 +37,6 @@ units = {"KB": 2**10, "MB": 2**20, "GB": 2**30}
     Ex. 8MB -> 8,192(KB)
 """
 
-
 def parse_mem_string(str):
     str = re.sub(r"([KMGT]?B)", r" \1", str)
 
@@ -44,38 +47,42 @@ def parse_mem_string(str):
 
     return int(float(number) * units[unit] / units["KB"])
 
-
 parser = argparse.ArgumentParser()
+required_args = parser.add_argument_group('required arguments')
 
-parser.add_argument(
+required_args.add_argument(
     "-f",
     action="append",
     default=[],
     metavar="filename",
     help="name of text file with the trace",
+    required=True
 )
-parser.add_argument(
+required_args.add_argument(
     "-s",
     type=int,
     metavar="[1, 8192]",
     help="denotes the cache size in KB from 1KB to 8MB",
+    required=True
 )
-parser.add_argument(
+required_args.add_argument(
     "-b",
     type=int,
     metavar="[4, 64]",
     help="denotes the block size in B from 4B to 64B",
+    required=True
 )
-parser.add_argument(
-    "-a", type=int, choices=[1, 2, 4, 8, 16], help="cache associativity"
+required_args.add_argument(
+    "-a", type=int, choices=[1, 2, 4, 8, 16], help="cache associativity", required=True
 )
-parser.add_argument(
-    "-r", choices=["RR", "RND"], help="replacement policy, either round-robin or random"
+required_args.add_argument(
+    "-r", choices=["RR", "RND"], help="replacement policy, either round-robin or random", required=True
 )
-parser.add_argument(
+required_args.add_argument(
     "-p",
     help="denotes the size of physical memory in KB from 64KB to 512GB",
     metavar="[64KB, 512GB]",
+    required=True
 )
 
 args = parser.parse_args()
@@ -88,7 +95,7 @@ if len(args.f) == 0 or len(args.f) > 3:
         "File quantity error: Provide at least one, at most three filename arguments"
     )
 
-if args.s < 0 or args.s > 8192:
+if args.s < 1 or args.s > 8192:
     parser.error("Cache size flag -s must be an integer value from 1 to 8192")
 
 if args.b < 4 or args.b > 64:
@@ -99,4 +106,5 @@ if physical_memory < 64 or physical_memory > upper_range:
         "Physical memory flag -p must be a string representing a memory size from 64 KB to 512 GB"
     )
 
-print(args)
+#part2.main(args)
+
