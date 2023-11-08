33fe823ae1695b9c92770b1208cd0446be04d623
me <91627805+agalindo96@users.noreply.github.com>
2023-11-07 22:43:42 -0600

    final touches

--- get_outfiles.sh
+++ get_outfiles.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python main.py -f trace1.trc -s 512 -b 16 -a 2 -r RR -p 1GB
+python main.py -f Trace1.trc -s 512 -b 16 -a 2 -r RR -p 1GB
+python main.py -f Trace1.trc -s 512 -b 16 -a 2 -r RR -p 1GB
+python main.py -f Trace1.trc -s 512 -b 16 -a 2 -r RR -p 1GB
--- milestone_one/output.py
+++ milestone_one/output.py
@@ -1,4 +1,16 @@
+import decimal
+import math
+
 def print_out(args):
+    total_blocks = (args.s * 1024) // args.b
+    tag_size = 32 - int(math.log2(args.b)) - int(math.log2(total_blocks // args.a))
+    index_size = int(math.log2(total_blocks // args.a))
+    total_rows = total_blocks // args.a
+    overhead_size = ((tag_size + 1) * args.a * total_rows) // 8
+    #TODO: Need units, float precision, value in bytes
+    memory_size = (overhead_size + args.s * 1024) / 1024.00
+    cost = memory_size * 0.09
+
     replacement = "Random" if args.r == "RND" else "Round-Robin"
 
     print("Cache Simulator CS 3853 Fall 2023 - Group #18\n")
@@ -12,10 +24,10 @@ def print_out(args):
         print(f"{'Replacement Policy: ':32}{replacement}\n\n")
 
         print("***** Cache Calculated Values *****\n")
-        print(f"{'Total # Blocks: ':32}#####")
-        print(f"{'Tag Size: ':32}## bits")
-        print(f"{'Index Size: ':32}## bits")
-        print(f"{'Total # Rows: ':32}####")
-        print(f"{'Overhead Size: ':32}##### bytes")
-        print(f"{'Implementation Memory Size: ':32}###.## #B (###### bytes)")
-        print(f"{'Cost: ':32}$##.## @ ($0.09 / KB)\n")
+        print(f"{'Total # Blocks: ':32}{total_blocks}")
+        print(f"{'Tag Size: ':32}{tag_size} bits")
+        print(f"{'Index Size: ':32}{index_size} bits")
+        print(f"{'Total # Rows: ':32}{total_rows}")
+        print(f"{'Overhead Size: ':32}{overhead_size} bytes")
+        print(f"{'Implementation Memory Size: ':32}{memory_size:.2f} KB ({int(memory_size * math.pow(2, 10))} bytes)")
+        print(f"{'Cost: ':32}${cost:.2f} @ ($0.09 / KB)\n")
